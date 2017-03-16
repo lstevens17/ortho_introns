c506e59bcd902bfb30eb588f479d97a69d24a071
Lewis Stevens <lewis.stevens07@gmail.com>
2017-03-16 15:39:19 +0000

    Add template script

---
+++ ortho_introns.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from os.path import basename, isfile, abspath, join
+import sys
+
+def test():
+   pass
+
+if __name__ == "__main__":
+
+   SCRIPT = basename(__file__)
+
+   infile, value, outfile = '', 0,''
+   try:
+       nucleotide_alignment = sys.argv[1]
+       DB_to_prefix = sys.argv[2]
+   except:
+       sys.exit("USAGE: ./%s %s %s" % (SCRIPT, "CDS ALIGNMENT", "DATABASE INFO"))
+
+   test()
