8f5d96811bbd99c082734f48a54d61e55125e26d
Lewis Stevens <lewis.stevens07@gmail.com>
2017-03-16 17:23:51 +0000

    Add print intron diagram

--- ortho_introns.py
+++ ortho_introns.py
@@ -38,16 +38,94 @@ def parse_species_DB ( species_to_DB ):
         prefix = line.strip("\n").split("\t")[0]
         DB = line.rstrip("\n").split("\t")[1]
         DB_dict[prefix] = DB
-        print DB_dict
-        return DB_dict
+    print DB_dict
+    return DB_dict
 
+## INSERT INTRONS INTO ALIGNMENT
+def insert_introns(fasta_dict, DB_dict):
+    intron_alignment_dict = {}
+    for key in fasta_dict: # each key represents one species
+        transcript_id = key
+        species_prefix = key.split(".")[0] # all sequence headers are prefixed with 5-letter species prefix
+        species_database = DB_dict[species_prefix]
+        with open(species_database + ".CDS_positions.txt", 'r') as CDS_positions: # parse the CDS positions
+            for line in CDS_positions:
+                if line.startswith(transcript_id):
+                    coord_list = line.rstrip("\n").split("\t")[1:] # we end up with a list of CDS lengths
+        aligned_sequence = fasta_dict[transcript_id]
+        base_number = 0
+        intron_number = 0
+        intron_containing_sequence = '' # this will store our new sequence
+        for base in aligned_sequence:
+            if not base == "-": # don't count dashes
+                base_number += 1
+                if base_number == int(coord_list[intron_number]):
+                    intron_containing_sequence += base
+                    intron_containing_sequence += "~"
+                    intron_number += 1
+                    base_number = 0 # this is because we are basing it off of CDS lengths, not intron positions
+                else:
+                    intron_containing_sequence += base
+            else:
+                intron_containing_sequence += base #this will be a -. but we want this in final dict
+        intron_alignment_dict[key] = intron_containing_sequence
+    ### JUST A PRINT LOOP, DELETE
+    for key in intron_alignment_dict:
+        print ">" + key
+        print intron_alignment_dict[key]
+    return intron_alignment_dict
+
+def print_intron_positions(intron_alignment_dict):
+    for key in intron_alignment_dict:
+        species_prefix = key.split(".")[0]
+        aligned_sequence = intron_alignment_dict[key]
+        alignment_position = 1
+        intron_positions = []
+        for position in aligned_sequence:
+                if position == "~":
+                        intron_positions.append(str(alignment_position))
+                else:
+                        alignment_position += 1
+        print species_prefix + "\t" + "\t".join(intron_positions)
+
+def print_intron_diagram(intron_alignment_dict):
+    all_intron_positions = []
+    for key in intron_alignment_dict:
+        aligned_sequence = intron_alignment_dict[key]
+        alignment_position = 1
+        for position in aligned_sequence:
+                if position == "~":
+                        if not alignment_position in all_intron_positions:
+                                all_intron_positions.append(str(alignment_position))
+                else:
+                        alignment_position += 1
+    for key in intron_alignment_dict:
+        species_prefix = key.split(".")[0]
+        aligned_sequence = intron_alignment_dict[key]
+        alignment_position = 1
+        intron_positions = []
+        for position in aligned_sequence:
+                if position == "~":
+                        intron_positions.append(str(alignment_position))
+                else:
+                        alignment_position += 1
+        binary = ""
+        for i in sorted(set(all_intron_positions)):
+                if i in intron_positions:
+                        binary += "|"
+                else:
+                        binary += "-"
+        print species_prefix + "\t" + binary
 
 # Run the code
 def run_software():
    with open(CDS_alignment_file, 'r') as CDS_alignment:
        with open(species_to_DB_file, 'r') as species_to_DB:
-           parse_alignment(CDS_alignment)
-           parse_species_DB(species_to_DB)
+           fasta_dict = parse_alignment(CDS_alignment)
+           DB_dict = parse_species_DB(species_to_DB)
+           intron_alignment_dict = insert_introns(fasta_dict, DB_dict)
+           print_intron_positions(intron_alignment_dict)
+           print_intron_diagram(intron_alignment_dict)
 
 if __name__ == "__main__":
 
